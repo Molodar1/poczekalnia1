21de440031445908a121dc810a462608e0802abf
Jakub Chmielewski <j.chmielewski1008@gmail.com>
2020-01-08 03:44:23 +0100

    Initial Commit

---
+++ src/main/java/pl/chmielewski/poczekalnia/PoczekalniaApplication.java
@@ -0,0 +1,13 @@
+package pl.chmielewski.poczekalnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoczekalniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoczekalniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/chmielewski/poczekalnia/PoczekalniaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.chmielewski.poczekalnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoczekalniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
